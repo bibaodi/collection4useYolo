65b60b64299cf60add2715251a7ad6e89722686f
eton bi <eton.bi@outlook.com>
2025-03-12 21:57:55 +0800

    1. add split data; 2. change cls train , set erasing=0.1

--- trainClassifyBoM_v01.py
+++ trainClassifyBoM_v01.py
@@ -1,5 +1,5 @@
 from ultralytics import YOLO
-
+import pathlib
 
 
 # - eton@250215 change dataset home from absolute to relative;
@@ -17,24 +17,29 @@ model = YOLO("yolo11n-cls.pt")  # load a pretrained model (recommended for train
 datasetHome=r'../datasets/'
 datasetName=r"301pacsDataInLbmfmtRangeY22-24.clsBoM_extend2times"
 datasetName=r"clsBoM_v01_extend2times"
-datasetName=r"clsBoM_v02"
+datasetName=r"clsBoM_v03test"
 trainMessage="2cd train to classify Benign Malign model, 8/2 for train/val, delete too small images"
+trainMessage="3rd test on dong5k images only"
 
 datasetf=datasetHome+datasetName
 
 # Train the model
 #results = model.train(data="mnist", epochs=10, imgsz=32)
-results = model.train(data=datasetf, epochs=90, imgsz=224)#96
+results = model.train(data=datasetf, epochs=1, imgsz=96, erasing=0.1)#96
 
 # Validate the model
 metrics = model.val()  # no arguments needed, dataset and settings remembered
 metrics.top1  # top1 accuracy
 
+# Export the model to ONNX format
+success = model.export(format="onnx")
 # Perform object detection on an image using the model
 #results = model("https://ultralytics.com/images/bus.jpg")
 tobeTestImg=datasetf+r'/test/malign/301PACS02-2201041312.01_frm-0002.png'
-results = model.predict(tobeTestImg)
-# Export the model to ONNX format
-success = model.export(format="onnx")
+testImg=pathlib.Path(tobeTestImg)
+if testImg.is_file():
+    results = model.predict(tobeTestImg)
+else:
+    print(f"test img not exist:{testImg}")
 print(f"export to ONNX: success={success}")
 
