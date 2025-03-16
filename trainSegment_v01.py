0ffc95d35c2f0bbb50006684a796439a91f17d78
eton bi <eton.bi@outlook.com>
2025-03-16 21:56:04 +0800

    add validate script, and update file names

--- trainSegment_v01.py
+++ trainSegment_v01.py
@@ -6,7 +6,7 @@ model = YOLO("yolo11n-seg.pt")
 # Train the model
 #results = model.train(data="coco8-seg.yaml", epochs=1, imgsz=640)
 #results = model.train(data="thyNoduSeg.yaml", epochs=1, imgsz=640)
-results = model.train(data="../datasets/thyNoduBoMSegV01/thynoduBoMSeg.yaml", epochs=120, imgsz=640)
+results = model.train(data="../datasets/thyNoduBoMSegV01/thynoduBoMSeg.yaml", epochs=3, imgsz=640)
 # Export the model to ONNX format
 success = model.export(format="onnx")
 print(f"export to ONNX: success={success}")
