f5fc0e68f2d1cb8d35e67c0ebe1b6d8eb6f48d55
eton bi <eton.bi@outlook.com>
2025-05-09 21:47:38 +0800

    support filter mask in box, remove others

--- predict_segNoCuttingToBox.py
+++ predict_segNoCuttingToBox.py
@@ -13,44 +13,7 @@ from ultralytics.engine.results import Results
 from ultralytics.utils import ASSETS
 import matplotlib.pyplot as plt
 
-def prune_components(img, min_area_per=0.002, deep_prune=True):
-    """
-    Post processing step to prune away small patches and ouput a convex
-    hull of (at most) the two largest patches
-
-    Args:
-        img (np.ndarray): nrow x ncol x nchan thresholded input mask
-        min_area_per (float): Minimum area percentage (i.e. patch area / image area) to be kept
-        deep_prune : False - only reomve area smaller than min_area
-
-    Returns:
-        np.ndarray: Output mask
-    """
-    # get contours from image
-    contours, hierarchy = cv2.findContours(
-                img, cv2.RETR_TREE,
-                cv2.CHAIN_APPROX_SIMPLE)
-    outimg = np.zeros(img.shape, dtype=np.uint8)
-    outimg = cv2.drawContours(outimg, contours, -1, 128, 3)
-    cv2.imwrite(f"/tmp/res-prune-contours-all.png", outimg)
-    
-    min_area = img.size * min_area_per
-    # sort contours by area
-    areas = np.array([cv2.contourArea(c) for c in contours])
-    print(f"debug: areas={areas}")
-    sortedAreaIdx = np.argsort(areas)
-    #areas = areas[sortedAreaIdx]
-    sorted_contours = []
-    # get rid of small contours
-    for icontourIdx in sortedAreaIdx:
-        if areas[icontourIdx]> min_area:
-            sorted_contours.append(contours[icontourIdx])
-            print(f"debug: areas[icontourIdx]={areas[icontourIdx]}, min_area={min_area}")
-            newmaskimg = np.zeros(img.shape, dtype=np.uint8)
-            cv2.drawContours(newmaskimg, [contours[icontourIdx]], -1, 253, -1)
-            cv2.imwrite(f"/tmp/resnewmaskimg-prune-{icontourIdx}.png", newmaskimg)
-    return newmaskimg
-
+debug_code = False
 def scale_masks(masks, shape, padding=True):
     """
     Rescale segment masks to shape.
@@ -76,96 +39,71 @@ def scale_masks(masks, shape, padding=True):
 
     masks = torchnnF.interpolate(masks, shape, mode="bilinear", align_corners=False)  # NCHW
     return masks
-def crop_mask(masks, boxes):
+def filter_mask_in_boxes(masks, boxes):
     """
-    Crop masks to bounding boxes.
+    filter masks to bounding boxes, remove others.
 
     Args:
         masks (torch.Tensor): [n, h, w] tensor of masks.
         boxes (torch.Tensor): [n, 4] tensor of bbox coordinates in relative point form.
 
     Returns:
-        (torch.Tensor): Cropped masks.
+        (torch.Tensor): masks in box.//currently only support 1 mask in 1 box.
     """
     _, h, w = masks.shape
     x1, y1, x2, y2 = torch.chunk(boxes[:, :, None], 4, 1)  # x1 shape(n,1,1)
-    print(f"debug: masks.shape={masks.shape}, boxes.shape={boxes.shape}, boxes={boxes}, x1={x1}, y1={y1}, x2={x2}, y2={y2}\n w={w}, h={h}")
-    for ii, imask in enumerate(masks):
-        print(f"debug: imask.shape={imask.shape}")
-        maskInnumpy = imask.cpu().numpy()
-        max_value = np.max(maskInnumpy)
-        min_value = np.min(maskInnumpy)
-        print(f"Maximum value: {max_value},Minimum value: {min_value}")
-        thresh = 0.8*(max_value + min_value)
-        maskInnumpy = (maskInnumpy > thresh).astype(np.uint8)
-        
-        maskInnumpy = prune_components(maskInnumpy, min_area_per=0.002, deep_prune=True)
-        contours, hierarchy = cv2.findContours(
-            (maskInnumpy > thresh).astype(np.uint8), cv2.RETR_TREE,
-            cv2.CHAIN_APPROX_SIMPLE)
-        print(f"debug: contours={type(contours)}, len(contours)={len(contours)}, contours[0].type={type(contours[0])}, contours[0].shape={contours[0].shape}")
-        cv2.imwrite(f"/tmp/resmask{ii}.png", maskInnumpy*255 )
-        outimg = cv2.cvtColor(maskInnumpy, cv2.COLOR_GRAY2BGR)
-        outimg = cv2.drawContours(outimg, contours, -1, (0, 255, 0), 3)
-        cv2.imwrite(f"/tmp/resmask{ii}-contours.png", outimg)
-        # Flatten the array
-        flattened_array = maskInnumpy.flatten().clip(0, 1)
-        # Plot the histogram
-        plt.hist(flattened_array, bins=10, alpha=0.7, color='blue')
-        plt.title('Value Distribution in 2D Array')
-        plt.xlabel('Value')
-        plt.ylabel('Frequency')
-
-        # Save the plot as an image
-        plt.savefig('/tmp/value_distribution.png')
-        plt.close()
-    boxW= x2 - x1
-    boxH= y2 - y1
-    extendRatio = 0.4
-    x1 = x1 - boxW * extendRatio
-    y1 = y1 - boxH * extendRatio
-    x2 = x2 + boxW * extendRatio
-    y2 = y2 + boxH * extendRatio
-    x1 = torch.clamp(x1, 0, w)
-    y1 = torch.clamp(y1, 0, h)
-    x2 = torch.clamp(x2, 0, w)
-    y2 = torch.clamp(y2, 0, h)
+    
     r = torch.arange(w, device=masks.device, dtype=x1.dtype)[None, None, :]  # rows shape(1,1,w)
     c = torch.arange(h, device=masks.device, dtype=x1.dtype)[None, :, None]  # cols shape(1,h,1)
+    box_mask = (r >= x1) * (r < x2) * (c >= y1) * (c < y2)  # box_mask shape(n,h,w)
+    box_mask0InNumpy = box_mask[0].numpy().astype(np.uint8)
+    if debug_code:
+        print(f"debug: box_mask.shape={box_mask.shape}")
+        cv2.imwrite(f"/tmp/boxmask.png", (box_mask[0].detach().cpu().numpy() * 255).astype(np.uint8)) 
+        print(f"debug: masks.shape={masks.shape}, boxes.shape={boxes.shape}, boxes={boxes}, x1={x1}, y1={y1}, x2={x2}, y2={y2}\n w={w}, h={h}")
+    
+    for ii, imask in enumerate(masks):
+        boxxyxy = boxes[ii]
+        print(f"debug: imask.shape={imask.shape}, boxxyxy={boxxyxy}")
+        maskInnumpy = imask.cpu().numpy()
+        if debug_code:
+            max_value = np.max(maskInnumpy)
+            min_value = np.min(maskInnumpy)
+            print(f"Maximum value: {max_value},Minimum value: {min_value}")
+        thresh = 0.0 #99*(max_value + min_value)
+        maskInnumpy = (maskInnumpy > thresh).astype(np.uint8)
+        
+        contours, hierarchy = cv2.findContours(
+            maskInnumpy, cv2.RETR_TREE,
+            cv2.CHAIN_APPROX_SIMPLE)
+        for idx4contour, iContour in enumerate(contours):
+            newmaskimg = np.zeros(maskInnumpy.shape, dtype=np.uint8)
+            cv2.drawContours(newmaskimg, [iContour], -1, 1, -1)
+            if np.sum(box_mask0InNumpy * newmaskimg) > 0:
+                if debug_code:
+                    print(f"debug: found a contour that is inside the box, sum={np.sum(box_mask0InNumpy * newmaskimg)}")
+                    cv2.imwrite(f"/tmp/maskInBox{idx4contour}.png", newmaskimg*245)
+                return torch.from_numpy(newmaskimg)
+    
+        if debug_code:
+            print(f"debug: contours={type(contours)}, len(contours)={len(contours)}, contours[0].type={type(contours[0])}, contours[0].shape={contours[0].shape}")
+            cv2.imwrite(f"/tmp/resmask{ii}.png", maskInnumpy*255 )
+            outimg = cv2.cvtColor(maskInnumpy, cv2.COLOR_GRAY2BGR)
+            outimg = cv2.drawContours(outimg, contours, -1, (0, 255, 0), 1)
+            cv2.imwrite(f"/tmp/resmask{ii}-contours.png", outimg)
+            # Flatten the array
+            flattened_array = maskInnumpy.flatten().clip(0, 1)
+            # Plot the histogram
+            plt.hist(flattened_array, bins=10, alpha=0.7, color='blue')
+            plt.title('Value Distribution in 2D Array')
+            plt.xlabel('Value')
+            plt.ylabel('Frequency')
 
-    return masks * ((r >= x1) * (r < x2) * (c >= y1) * (c < y2))
+            # Save the plot as an image
+            plt.savefig('/tmp/value_distribution.png')
+            plt.close()
 
-def process_mask(protos, masks_in, bboxes, shape, upsample=False):
-    """
-    Apply masks to bounding boxes using the output of the mask head.
-
-    Args:
-        protos (torch.Tensor): A tensor of shape [mask_dim, mask_h, mask_w].
-        masks_in (torch.Tensor): A tensor of shape [n, mask_dim], where n is the number of masks after NMS.
-        bboxes (torch.Tensor): A tensor of shape [n, 4], where n is the number of masks after NMS.
-        shape (tuple): A tuple of integers representing the size of the input image in the format (h, w).
-        upsample (bool): A flag to indicate whether to upsample the mask to the original image size.
-
-    Returns:
-        (torch.Tensor): A binary mask tensor of shape [n, h, w], where n is the number of masks after NMS, and h and w
-            are the height and width of the input image. The mask is applied to the bounding boxes.
-    """
-    c, mh, mw = protos.shape  # CHW
-    ih, iw = shape
-    masks = (masks_in @ protos.float().view(c, -1)).view(-1, mh, mw)  # CHW
-    width_ratio = mw / iw
-    height_ratio = mh / ih
-
-    downsampled_bboxes = bboxes.clone()
-    downsampled_bboxes[:, 0] *= width_ratio
-    downsampled_bboxes[:, 2] *= width_ratio
-    downsampled_bboxes[:, 3] *= height_ratio
-    downsampled_bboxes[:, 1] *= height_ratio
-
-    #masks = crop_mask(masks, downsampled_bboxes)  # CHW
-    if upsample:
-        masks = torchnnF.interpolate(masks[None], shape, mode="bilinear", align_corners=False)[0]  # CHW
-    return masks.gt_(0.0)
+    return masks 
 
 class YOLOSeg:
     """
@@ -282,34 +220,6 @@ class YOLOSeg:
         img = img.astype(np.float32) / 255  # Normalize to [0, 1]
         return img
 
-    def postprocess(self, img, prep_img, outs):
-        """
-        Post-process model predictions to extract meaningful results.
-
-        Args:
-            img (np.ndarray): The original input image.
-            prep_img (np.ndarray): The preprocessed image used for inference.
-            outs (list): Model outputs containing predictions and prototype masks.
-
-        Returns:
-            (List[Results]): Processed detection results containing bounding boxes and segmentation masks.
-        """
-        
-        preds, protos = [torch.from_numpy(p) for p in outs]
-        print(f"debug: {type(preds)}preds.shape={preds.shape}, protos.shape={protos.shape}")
-        model_names={0: 'thyNodu'} 
-        preds = ops.non_max_suppression(preds, self.conf, self.iou, None, False, max_det=3000, nc=len(model_names), end2end=False, return_idxs=False)
-        print(f"debug: after apply nms: {type(preds)},{len(preds)},preds0.shape={preds[0].shape}, protos.shape={protos.shape}")
-        results = []
-        for i, pred in enumerate(preds):
-            pred[:, :4] = ops.scale_boxes(prep_img.shape[2:], pred[:, :4], img.shape)#0-3:wxywh,4:conf,5:cls;
-            print(f"debug: pred.shape={pred.shape},pred[:5]={pred[:, :5]},0-3:wxywh,4:conf,5:cls")
-            #masks = process_mask(protos[i], pred[:, 6:], pred[:, :4], img.shape[:2])
-            masks = self.process_mask(protos[i], pred[:, 6:], pred[:, :4], img.shape[:2])
-            results.append(Results(img, path="", names={'nodu':"Nodule"}, boxes=pred[:, :6], masks=masks), )
-
-        return results
-
     def process_mask(self, protos, masks_in, bboxes, shape):
         """
         Process prototype masks with predicted mask coefficients to generate instance segmentation masks.
@@ -324,12 +234,41 @@ class YOLOSeg:
             (torch.Tensor): Binary segmentation masks with shape (n, height, width).
         """
         c, mh, mw = protos.shape  # CHW
-        print(f"debug: protos.shape={protos.shape}, masks_in.shape={masks_in.shape}")
+        #print(f"debug: protos.shape={protos.shape}, masks_in.shape={masks_in.shape}")
         masks = (masks_in @ protos.float().view(c, -1)).view(-1, mh, mw)  # Matrix multiplication
         masks = scale_masks(masks[None], shape)[0]  # Scale masks to original image size
-        print(f"debug: after scale mask: masks.shape={masks.shape}, bboxes={bboxes}")
-        masks = crop_mask(masks, bboxes)  # Crop masks to bounding boxes
+        #print(f"debug: after scale mask: masks.shape={masks.shape}, bboxes={bboxes}")
+        masks = filter_mask_in_boxes(masks, bboxes)  # Crop masks to bounding boxes
         return masks.gt_(0.0)  # Convert to binary masks
+    
+    def postprocess(self, img, prep_img, outs):
+        """
+        Post-process model predictions to extract meaningful results.
+
+        Args:
+            img (np.ndarray): The original input image.
+            prep_img (np.ndarray): The preprocessed image used for inference.
+            outs (list): Model outputs containing predictions and prototype masks.
+
+        Returns:
+            (List[Results]): Processed detection results containing bounding boxes and segmentation masks.
+        """
+        
+        preds, protos = [torch.from_numpy(p) for p in outs]
+        #print(f"debug: {type(preds)}preds.shape={preds.shape}, protos.shape={protos.shape}")
+        model_names={0: 'thyNodu'} 
+        preds = ops.non_max_suppression(preds, self.conf, self.iou, None, False, max_det=3000, nc=len(model_names), end2end=False, return_idxs=False)
+        #print(f"debug: after apply nms: {type(preds)},{len(preds)},preds0.shape={preds[0].shape}, protos.shape={protos.shape}")
+        results = []
+        for i, pred in enumerate(preds):
+            pred[:, :4] = ops.scale_boxes(prep_img.shape[2:], pred[:, :4], img.shape)#0-3:wxywh,4:conf,5:cls;
+            #print(f"debug: pred.shape={pred.shape},pred[:5]={pred[:, :5]},0-3:wxywh,4:conf,5:cls")
+            masks = self.process_mask(protos[i], pred[:, 6:], pred[:, :4], img.shape[:2])
+            results.append(Results(img, path="", names={'nodu':"Nodule"}, boxes=pred[:, :6], masks=masks), )
+
+        return results
+
+
 
 
 if __name__ == "__main__":
@@ -356,7 +295,7 @@ for result in results:
 
     img = np.copy(result.orig_img)
     b_mask = np.zeros(img.shape[:2], np.uint8)
-    print(f"debug: {type(masks.xy)}, len={len(masks.xy)}, masks={masks}") if len(masks.xy) <= 0 else print("a lot masks found")
+    #print(f"debug: {type(masks.xy)}, len={len(masks.xy)}, masks={masks}") if len(masks.xy) <= 0 else print("a lot masks found")
     contour = masks.xy[0].astype(np.int32).reshape(-1, 1, 2)
     cv2.drawContours(b_mask, [contour], -1, (255, 255, 255), cv2.FILLED)
     isolated = np.dstack([img, b_mask])
